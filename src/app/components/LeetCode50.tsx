46e9d5387a20a77d8c43f5e08dfc7dda425f779a
Sagar Khatri <sagarkhatri.pro@gmail.com>
2025-11-06 22:01:19 +0530

    removed useless code

--- src/app/components/LeetCode50.tsx
+++ src/app/components/LeetCode50.tsx
@@ -7,7 +7,7 @@ export default function LeetCode50() {
 	const isGhDeployment: boolean = process.env.NODE_ENV === "production";
 	const basePath: string = repoBasePath;
 	return (
-		<Link className="flex flex-col items-center hover:cursor-pointer" href={(isGhDeployment ? basePath : "") + "/leetcode50.md"}>
+		<Link className="flex flex-col items-center hover:cursor-pointer" href="/leetcode50.md">
 			<div className="rounded-xl border w-56 h-56 p-5 hover:p-1 bg-black bg-opacity-50 transition-all duration-300 overflow-hidden flex">
 				<Image className="my-auto rounded-xl" src={(isGhDeployment ? basePath : "") + "/images/leetcode.png"} alt={"leetcode50"} width={224} height={224} objectFit="cover" />
 			</div>
