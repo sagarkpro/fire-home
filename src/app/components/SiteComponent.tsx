8158828748437cd18dd2cae01df8d32647316688
Sagar Khatri <sagarkhatri.pro@gmail.com>
2025-12-18 23:45:28 +0530

    mobile view improvements

--- src/app/components/SiteComponent.tsx
+++ src/app/components/SiteComponent.tsx
@@ -10,10 +10,10 @@ function SiteComponent({ siteData }: { siteData: SiteData }) {
 
     return (
         <Link className="flex flex-col items-center hover:cursor-pointer" href={siteData.RedirectUrl} target="_blank">
-            <div className="rounded-xl border w-56 h-56 p-5 hover:p-1 bg-black bg-opacity-50 transition-all duration-300 overflow-hidden flex">
+            <div className="rounded-xl border w-28 md:w-56 h-28 md:h-56 p-5 hover:p-1 bg-black bg-opacity-50 transition-all duration-300 overflow-hidden flex">
                 <Image className="my-auto rounded-xl" src={((isGhDeployment ? basePath : "") + siteData.ImageUrl)} alt={siteData.Title} width={siteData.Width ?? 224} height={siteData.Height ?? 224} objectFit="cover" />
             </div>
-            <div className="font-bold text-xl my-4">
+            <div className="font-bold md:text-xl my-4">
                 {siteData.Title}
             </div>
         </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,11 +59,11 @@ export default function Home() {
 	return (
 		<div className={`w-full h-svh overflow-clip bg-cover bg-center ${isAnime ? backgroundImage : "bg-black"}`}>
 			<div className={`w-full h-full overflow-y-scroll overflow-x-clip flex flex-col items-center p-8`}>
-				<LocalhostPortLauncher />
-				
-				
-				<div className="m-2 mb-16 w-full flex flex-col items-center md:flex-row md:justify-evenly">
-					<div className="w-1/3 flex items-center">
+				<div className="gap-x-4 md:gap-0 m-2 mb-16 w-full flex items-center flex-wrap md:flex-nowrap justify-between px-6 md:px-0 md:justify-evenly">
+					<div className="md:hidden w-full flex justify-center">
+						<Firefox></Firefox>
+					</div>
+					<div className="md:w-1/3 flex items-center">
 						<div className="flex gap-x-0.5 rounded-3xl overflow-clip">
 							<button onClick={() => changeCurrentView("tools")} className={`w-10 bg-indigo-200 p-2 border-black ${currentView == "tools" ? "opacity-100" : "opacity-70"}`}>
 								<Image width={32} height={32} src={(isGhDeployment ? basePath : "") + "/images/tools.svg"} alt="Tools" />
@@ -74,19 +74,18 @@ export default function Home() {
 						</div>
 					</div>
 
-					<div className="md:w-1/3 flex justify-center">
+					<div className="hidden md:flex w-1/3 justify-center">
 						<Firefox></Firefox>
 					</div>
 
-					<div className="flex md:w-1/3 items-center justify-end px-12">
+					<div className="flex md:w-1/3 items-center justify-end md:px-12">
 						<button className="rounded-full bg-zinc-50 text-black p-1 w-[5.5rem]" onClick={changeTheme}>
 							<div className={`rounded-[5rem] w-10 h-10 transition-all duration-150 bg-cover bg-center ${isAnime ? buttonImage : "bg-black"}`} style={{ transform: `translateX(${translateX})` }}></div>
 						</button>
 					</div>
 				</div>
 
-
-				<div id="content-div" className="flex max-w-screen-2xl min-h-svh flex-wrap justify-center">
+				<div id="content-div" className="flex max-w-screen-2xl flex-wrap justify-center">
 					{currentView == "shortcuts" &&
 						sites.map((site) => {
 							return (
@@ -103,6 +102,7 @@ export default function Home() {
 					{currentView == "tools" && <ToolsWrapper />}
 				</div>
 			</div>
+			<LocalhostPortLauncher />
 		</div>
 	);
 }
